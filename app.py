238d97c2ff78a90987a4e2e8de6b431029bc78c3
Megumi1710 <brijnandankumar157@gmail.com>
2026-05-12 20:17:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-import streamlit as st 
-st.set_page_config(page_title="Study Assist"
-page_icon="😉😎💕"
-layout="wide")
+import streamlit as st
+
+# Import dashboard function
+from pages.dashboard import show_dashboard
+
+
+# App configuration
+st.set_page_config(
+    page_title="StudySync AI",
+    page_icon="📚",
+    layout="wide"
+)
+
+
+# Sidebar
 with st.sidebar:
-  st.title("Study Assist Space")
-page= st.radio("Navigation",["Dashboard","Study Tracker","Analytics","AI Assistance"])
-if page== "Dashboard":
-  st.title("Dashboard")
-  st.subheader("Welcome Back!!!")
-  st.columns(3)= col1, col2, col3
-  with col1:
-    st.metric("Study Hours", "12")
- with col2:
-  st.metric("Attendence","87%")
-  with col3:
-    st.metric("Streak", "5 days")
-    st.divider()
+
+    st.title("📚 StudySync AI")
+
+    page = st.radio(
+        "Navigation",
+        ["Dashboard"]
+    )
+
+
+# Page Routing
+if page == "Dashboard":
+    show_dashboard()
